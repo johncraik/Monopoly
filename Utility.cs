5946e497c30d712c31cd48687ea02058aee55bd2
johncraik <jgcraik1@sheffield.ac.uk>
2023-11-04 16:44:43 +0000

    Added property classes and subclasses

---
+++ Utility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyApp
+{
+    internal class Utility : Properties
+    {
+    }
+}
