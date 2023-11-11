e9cddeca703de5493d2161d3a6599fc8c580c6a2
johncraik <jgcraik1@sheffield.ac.uk>
2023-11-11 16:41:05 +0000

    Removed individual coloured properties classes and added a Coloured class

---
+++ Coloured.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyApp
+{
+    internal class Coloured : Properties
+    {
+        /*Class Attributes:
+         * houses       {int}   Number of houses built on the property.
+         * hotel        {bool}  Whether the property has a hotel on it (5 houses).
+         * buildCost    {int}   Cost to build a house/hotel.
+         * set          {bool}  Whether this property is in a set.
+         * 
+        */
+    }
+}
