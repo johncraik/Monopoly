ace5b7489b033d64f49b4b696b4d43eaf4b6b6d3
johncraik <jgcraik1@sheffield.ac.uk>
2023-11-11 17:00:47 +0000

    Added attributes and constructor to Utility class

--- Utility.cs
+++ Utility.cs
@@ -8,5 +8,22 @@ namespace MonopolyApp
 {
     internal class Utility : Properties
     {
+        /*Class Attributes:
+         * set          {bool}  Whether this property is in a set.
+         * rentSingle   {int}   Multiplier value for rent for owning 1 utility.
+         * rentMultiple {int}   Multiplier value for rent for owning both utilities.
+        */
+        private bool set = false;
+        private readonly int rentSingle = 4;
+        private readonly int rentMultiple = 10;
+
+
+        //Constructor:
+        public Utility(Board board, int x, int y, string colour,    //Squares class attributes.
+            string name, int cost, int baseRent)                    //Properties class attributes.
+            :base(board, x, y, colour, name, cost, baseRent)        //Attributes passed to superclass.
+        {
+            this.set = false;
+        }
     }
 }
